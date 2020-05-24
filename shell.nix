a067d163b5902799d63c281e43c05a22dd2a331f
maxild <mmaxild@gmail.com>
2020-05-24 15:36:21 +0200

    Patched SPIM 32-bit to work using patchelf

---
+++ shell.nix
@@ -0,0 +1,30 @@
+let
+  pkgs = import <nixpkgs> { system = "i686-linux"; };
+
+  #libcPath = makeLibraryPath
+  libcPath = "${pkgs.stdenv.cc.libc}/lib";
+in
+  pkgs.mkShell {
+    name = "cs143-class";
+
+    nativeBuildInputs = [
+      pkgs.patchelf
+    ];
+
+    buildInputs = [
+      pkgs.gcc
+      pkgs.gnumake
+      pkgs.flex
+      pkgs.bison
+    ];
+
+    shellHook = ''
+      export PATH="$PATH:$PWD/bin"
+      #export LD_LIBRARY_PATH=${libcPath}
+      # run SPIM with 32-bit ELF interpreter from the nix store
+      patchelf --set-interpreter ${libcPath}/ld-linux.so.2 \
+               --set-rpath ${libcPath} \
+               $PWD/bin/.i686/spim
+    '';
+
+  }
