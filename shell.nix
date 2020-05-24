026ee9cc799e1231767a09ca6bad8fb05599eb42
maxild <mmaxild@gmail.com>
2020-05-24 16:37:24 +0200

    Added guard in shell.script such that patchelf is run only once

--- shell.nix
+++ shell.nix
@@ -22,9 +22,12 @@ in
       export PATH="$PATH:$PWD/bin"
       #export LD_LIBRARY_PATH=${libcPath}
       # run SPIM with 32-bit ELF interpreter from the nix store
-      patchelf --set-interpreter ${libcPath}/ld-linux.so.2 \
-               --set-rpath ${libcPath} \
-               $PWD/bin/.i686/spim
+      interp="$(patchelf --print-interpreter $PWD/bin/.i686/spim)"
+      if [[ ! ($interp =~ ^/nix/store) ]]; then
+        patchelf --set-interpreter ${libcPath}/ld-linux.so.2 \
+                 --set-rpath ${libcPath} \
+                 $PWD/bin/.i686/spim
+      fi
     '';
 
   }
